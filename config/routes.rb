c7b4bb691ca0d29d961a64287e711fe87400630d
martafonda <martafondapascual@gmail.com>
2013-11-11 15:49:55 +0000

    Change index root to movies route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Manderlay::Application.routes.draw do
   resources :movies
-
+  root 'movies#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
