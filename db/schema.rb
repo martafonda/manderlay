0edbc733d76704b94f8475b2e9365fa88fe77861
martafonda <martafondapascual@gmail.com>
2013-11-13 09:52:17 +0000

    added people model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112102328) do
+ActiveRecord::Schema.define(version: 20131113094318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20131112102328) do
     t.text     "synopsis"
     t.integer  "year"
     t.string   "category"
+  end
+
+  create_table "people", force: true do |t|
+    t.string   "name"
+    t.string   "surname"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
