f7dfac583c293a23b9bd3276bda7cb72116fe9d2
martafonda <martafondapascual@gmail.com>
2013-11-15 17:15:36 +0000

    scope changes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,11 @@ class Movie < ActiveRecord::Base
 	has_many :people, through: :casts
 	has_many :casts, :dependent => :destroy
 	validates :title, uniqueness: { scope: [:duration, :synopsis, :year, :category]}
+	validates_presence_of :title
+
+	
+	scope :bigger_than_duration, ->(duration) { where('duration > ?', duration)}
+	scope :smaller_than_duration, ->(duration) { where('duration < ?', duration)}
+	scope :bigger_than_year, ->(year) { where('year > ?', year)}
+	scope :smaller_than_year, ->(year) { where('year < ?', year)}
 end
